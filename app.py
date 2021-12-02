3a65aee641e2723f7e7088b68e70efbe4fdb00ec
annieptba <61258785+annieptba@users.noreply.github.com>
2021-12-01 20:01:03 -0500

    as

--- app.py
+++ app.py
@@ -9,9 +9,11 @@ import pandas as pd
 
 df = pd.read_csv('https://bit.ly/elements-periodic-table')
 
+# create columns that have unique values
 unique_col = ["AtomicNumber", "Element", "Symbol", "NumberOfProtons", "NumberOfElectrons"]
 
-not_unique_col = ['AtomicMass', 'NumberOfNeutrons', 'Period', 'Group', 'Phase', 'Radioactive', 'Natural', 'Metal', 'Nonmetal',
+# create columns that don't have unique values
+not_unique_col = ['AtomicMass', 'NumberOfNeutrons', 'Period', 'Group', 'Phase', 'Radioactive', 'Natural', 'Metal', 'Nonmetal', 
                   'Metalloid', 'Type', 'AtomicRadius', 'Electronegativity', 'FirstIonization', 'Density', 'MeltingPoint',
                   'BoilingPoint', 'NumberOfIsotopes', 'Discoverer', 'Year', 'SpecificHeat', 'NumberOfShells', 'NumberOfValence']
 
@@ -19,31 +21,42 @@ app = dash.Dash(__name__)
 
 # make app layout
 app.layout = html.Div([
-   dcc.Dropdown(
+   html.Div([
+      dcc.Dropdown(
       id='index_selection',
       options=[{'label': 'select an index', 'value': col} for col in unique_col],
       value=None
-      ), html.Div(id='index-output')
-   dcc.Dropdown(
+      )
+   ]), 
+   html.Div(id='index-output'),
+   html.Div([
+      dcc.Dropdown(
       id='column_selection',
       options=[{'label': 'select a colum', 'value': col} for col in unique_col],
       value=None
-      ), html.Div(id='column-output')
-   dcc.Dropdown(
+      )
+   ]),
+   html.Div(id='column-output'),
+   html.Div([
+      dcc.Dropdown(
       id='value_selection',
       options=[{'label': 'select a value', 'value': col} for col in not_unique_col],
       value=None
-      ), html.Div(id='value-output'),
-   dash_table.DataTable(
-   id='table',
-   columns=[{"name": i, "id": i} for i in df.columns],
-   data=df.to_dict('records')
-)
+      )
+   ]),
+   html.Div(id='value-output'),
+   html.Div([
+      dash_table.DataTable(
+      id='pivot-table',
+      columns=[{"name": i, "id": i} for i in df.columns],
+      data=df.to_dict('records')
+      )
+   ])
 ])
 
 # app callback
 @app.callback(
-   Output(component_id='pivot_table', component_property='rows'),
+   Output(component_id='pivot_table', component_property='data'),
    [
       Input(component_id='index_selection', component_property='value'),
       Input(component_id='column_selection', component_property='value'),
@@ -55,19 +68,19 @@ app.layout = html.Div([
 def identity(x): return x
 
 # make function to make pivot table
-def update_table(df, index_col, columns_col, values_col):
+def output_table(df, index_col, columns_col, values_col):
    not_unique_cols = [ col for col in df.columns if df[col].is_unique == False]
    if index_col == columns_col or index_col == values_col or columns_col ==values_col:
       print("row and column index needs to be different")
    else:
       df_piv = df.pivot_table(
             index= index_col,
-            columns= columns_col,
+            columns= columns_col, 
             values= values_col,
             aggfunc=identity,
       )
       return df_piv
 
-# run app
+# run app 
 app.run_server(debug=True, host="0.0.0.0")
 
